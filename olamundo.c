fe6987abc51afbef1021dcd78d56d2a9b459b3ba
Pietroehrich7 <pietrogarcez.riosaude@gmail.com>
2025-05-19 07:52:22 -0300

    Update olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+char nome(){
+  printf("Pietro Giannini Garcez Ehrich\n");
+}
+int idade(){
+  printf("21y\n");
+}
+float altura(){
+  printf("1,70\n");
+}
+double peso(){
+  printf("71,333333");
+return 0;
+}
